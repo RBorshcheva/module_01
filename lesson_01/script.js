89dc16d6ba75aadc840a9fa31d5031513a9333f8
Raisa Grishina <bbbaaa987@mail.ru>
2025-01-14 15:33:31 +0300

    initial

---
+++ lesson_01/script.js
@@ -0,0 +1,10 @@
+//наименование товара
+const productTitle = "Люстра";
+//количество товара
+const productCount = 5;
+//категория товара
+const productCategory = "Светильники";
+//цена товара
+const productPrice = 1000;
+
+console.log("Наименование товара: ", productTitle);
